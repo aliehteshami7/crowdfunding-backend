b9656b725457fc0d048b5f5263cb0adb9e728189
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-08-27 18:54:53 +0430

    Add signup api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,20 +15,36 @@ import { UserRo } from 'src/users/dto/user.ro';
 import { TokenRo } from './dto/token.ro';
 import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+import { UserCreateDto } from 'src/users/dto/user-create.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(@Inject(AuthService) private readonly authService: AuthService) {}
+  constructor(
+    @Inject(AuthService)
+    private readonly authService: AuthService,
+    @Inject(UsersService)
+    private readonly usersService: UsersService,
+  ) {}
 
   @Post()
   @HttpCode(200)
   @ApiBody({ type: LoginDto })
   @UseGuards(LocalAuthGuard)
   @ApiOperation({ summary: 'Login' })
-  @ApiOkResponse({ type: UserRo })
+  @ApiOkResponse({ type: LoginRo })
   async login(@Request() { user }: { user: UserRo }): Promise<LoginRo> {
     return await this.authService.login(user);
+  }
+
+  @Post('signup')
+  @ApiBody({ type: UserCreateDto })
+  @ApiOperation({ summary: 'Login' })
+  @ApiOkResponse({ type: LoginRo })
+  async signup(@Body() userCreateDto: UserCreateDto): Promise<LoginRo> {
+    const user = await this.usersService.create(userCreateDto);
+    return await this.login({ user });
   }
 
   @Post('verifyToken')
