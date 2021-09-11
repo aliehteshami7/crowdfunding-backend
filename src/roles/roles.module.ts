40fcbb858f7551b989ce41f226ea5c521a565146
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-09-11 23:51:24 +0430

    Fix module imports

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -3,10 +3,12 @@ import { RolesController } from './roles.controller';
 import { Module } from '@nestjs/common';
 import { Role, RoleSchema } from './schemas/role.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }]),
+    UsersModule,
   ],
   controllers: [RolesController],
   providers: [RolesService],
