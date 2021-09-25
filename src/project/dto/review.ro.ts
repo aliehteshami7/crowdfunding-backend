eeea36897dc4d7decfd421492c39dfe560a3f430
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-09-26 00:47:43 +0330

    Expose review ro fields

--- src/project/dto/review.ro.ts
+++ src/project/dto/review.ro.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 import { UserRo } from 'src/users/dto/user.ro';
 
 export class ReviewRo {
   @ApiProperty()
+  @Expose()
   public readonly score: number;
 
   @ApiProperty()
+  @Expose()
   public readonly text: string;
 
   @Type(() => UserRo)
   @ApiProperty()
+  @Expose()
   public readonly reviewer: UserRo;
 }
