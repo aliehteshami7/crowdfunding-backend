c1c8dd0e48608d42c2718b67a1fd5fa91be48962
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-10-18 10:56:22 +0330

    Add id field to reviews

--- src/project/dto/review.ro.ts
+++ src/project/dto/review.ro.ts
@@ -3,6 +3,11 @@ import { Expose, Type } from 'class-transformer';
 import { UserRo } from 'src/users/dto/user.ro';
 
 export class ReviewRo {
+  @Expose({ name: '_id' })
+  @Type(() => String)
+  @ApiProperty()
+  public id: string;
+
   @ApiProperty()
   @Expose()
   public readonly score: number;
